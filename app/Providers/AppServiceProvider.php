5509446ffcb54dae33137b5d3b17d72235bf8b6b
tothmarci25 <tothmarci25@gmail.com>
2022-07-24 14:26:12 +0200

    feat: change default schema string length for older mysql version

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Services\ResponseSerializer;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +28,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         JsonResource::withoutWrapping();
+
+        Schema::defaultStringLength(191);
 
         Response::macro('serializeAsRequested', function ($value, $xmlRootNodeName = null) {
             $serializer = new ResponseSerializer();
